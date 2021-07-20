9adfef29fd82655d48d2ccf3ee6c1f64b7f7fb8a
Lillo <sdgfdsgsdgds>
2021-07-20 12:26:40 -0300

    Initial Commit

---
+++ LearningGH/LearningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGHApp.swift
+//  LearningGH
+//
+//  Created by Murillo on 20/7/21.
+//
+
+import SwiftUI
+
+@main
+struct LearningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
